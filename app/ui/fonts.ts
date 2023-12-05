f12d2beae6841e4fd1a7b390fe326f2409202d0b
aricodewibowo04 <aricodewibowo@gmail.com>
2023-12-06 00:15:44 +0700

    add some chane file

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Merriweather } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin']
+});
+
+export const merriWeather = Merriweather({
+    weight: ['300', '700'],
+    subsets: ['latin'],
+});
